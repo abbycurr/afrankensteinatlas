25497e0ec889b22610c446974da12265846ee0e0
abbycurr <61121832+abbycurr@users.noreply.github.com>
2020-03-09 21:18:05 -0400

    Update Essay 1 - Marriage to a Monster.php

--- Essay 1 - Marriage to a Monster.php
+++ Essay 1 - Marriage to a Monster.php
@@ -1,1 +1,31 @@
 Essay 1 - Marriage to a Monster
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gender Theory Essay</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+<div>
+<h1> Gender Theory Essay</h1>
+<h2> Marriage to a Monster </h2>
+<p> Women are consistently portrayed as flat and unemotional characters in Frankenstein, in direct contrast to both Victor Frankenstein's volitile emotional state and also in contrast to the prevailing theories at the time about female emotional instability and hysteria. These stable female characters also provide a contrast to Mary Shelley's own adventurous and arguably outlandish entrance into both adulthood and motherhood. This argue will explore these contrasts and argue that Mary Shelley created a world in which the female characters lived stable and consistent lives to compensate for her own wild upbringing. Specifically, we will examine the lives of Elizabeth, Safie and Margaret with a heavy focus on their marital status. </p>
+
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
