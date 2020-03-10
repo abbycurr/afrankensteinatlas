e7c07fad1fa283ff98f51f4a66f059b34e276ff3
abbycurr <61121832+abbycurr@users.noreply.github.com>
2020-03-09 21:17:29 -0400

    Create Essay 2 - Nazis Build Frankenstein.php

---
+++ Essay 2 - Nazis Build Frankenstein.php
@@ -0,0 +1,32 @@
+Essay 2 - Nazis Build Frankenstein
+<!DOCTYPE html>
+<html>
+<head>
+<title>Individual Choice Essay</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<!-- Header Section -->
+<?php include("header.php"); 
+?>
+<div>
+    <h1> Individual Choice Essay </h1>
+    <h2> Nazis Build Frankenstein </h2>
+    <p> During World War II, all sides were focused on the best way to quickly win the war in their favor. This involved a lot of experimentation with various technologies. One area that the Nazis exolored was the development of the superhuman through different drugs. Victor Frankenstein was likewise focused on creating a new, superior race that would improve upon the current model. This essay will explore the human fascination with creating a super race and the different motivations behind doing so with a specific focus on the scientific movements of the late 1700's/early 1800's and the early 1900's. Arguably, the end goal did not change in over a hundred years, but the methods and motivations changed, which wil be the subject of this essay. </p>
+</div>
+
+
+<!-- Footer Section -->
+<?php include("footer.php"); 
+?>
+
+<!-- End page content -->
+</div>
+
+
+
+
+</body>
+</html>
